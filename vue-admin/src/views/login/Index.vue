fe2aacae5723420ab5aed39e6edd91f898298a35
emolly <2624658096@qq.com>
2021-05-30 22:17:38 +0800

    添加了样式

--- vue-admin/src/views/login/Index.vue
+++ vue-admin/src/views/login/Index.vue
@@ -1,14 +1,48 @@
 <template>
-  <div id="login">这里是登录页面</div>
+  <div id="login">
+      <div class="login-wrap">
+          <ul class="menu-tab">
+              <li class="current">登录</li>
+              <li>注册</li>
+          </ul>
+      </div>
+  </div>
 </template>
 <script>
+
 export default {
-    name: 'login'
+    name: 'login',
+    data(){
+        return {
+
+        }
+    },
+    created(){},
+    mounted(){},
+    methods:{}
 }
 </script>
 <style lang="scss" scoped>
  #login{
      background-color: blueviolet;
      height: 100vh;
+ }
+ .login-wrap{
+     width: 330px;
+     margin: auto;
+ }
+ .menu-tab{
+     text-align: center;
+     li{
+         display: inline-block;
+         width: 88px;
+         line-height: 36px;
+         font-size: 14px;
+         color: #fff;
+         border-radius: 2px;
+     }
+     .current{
+         background-color: rgba($color: #000000, $alpha: 1.0);
+     }
  }
 </style>
