c2227a824fff4e59a4485ee77ec982a2c523668e
emolly <2624658096@qq.com>
2021-05-30 21:49:39 +0800

    创建项目，下载eleementUI,

---
+++ vue-admin/src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+
+
+const routes = [
+  {
+    path: "/",
+    redirect: "login"
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () =>
+      import("../views/login/Index.vue")
+  },
+  
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
